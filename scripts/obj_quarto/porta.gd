cd9711f88b6cf133627cd8101e58620c692e5748
lliazc <liviaakkj@gmail.com>
2025-08-25 22:32:11 -0300

    confgurando fases e objetos

---
+++ scripts/obj_quarto/porta.gd
@@ -0,0 +1,13 @@
+extends Area2D  # Ou Node2D se preferir
+
+@export var target_scene: String = "res://cenas/corredor.tscn"
+
+func _ready():
+	# Conecta os sinais se for uma Area2D
+	if self is Area2D:
+		input_event.connect(_on_input_event)
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if target_scene != "":
+			get_tree().change_scene_to_file(target_scene)
---
+++ scripts/quarto_1.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var scene_to_load: String = "res://cenas/corredor.tscn"
+@onready var hover_label: Label = $Label
+
+func _ready():
+	# Conecta os sinais da Area2D
+	$mala.mouse_entered.connect(_on_mouse_entered)
+	$mala.mouse_exited.connect(_on_mouse_exited)
+	$mala.input_event.connect(_on_input_event)
+
+
+	$porta.mouse_entered.connect(_on_mouse_entered)
+	$porta.mouse_exited.connect(_on_mouse_exited)
+	$porta.input_event.connect(_on_input_event)
+
+func _on_mouse_entered():
+	if hover_label:
+		hover_label.visible = true
+
+func _on_mouse_exited():
+	if hover_label:
+		hover_label.visible = false
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if scene_to_load != "res://cenas/corredor.tscn":
+			get_tree().change_scene_to_file("res://cenas/corredor.tscn")
